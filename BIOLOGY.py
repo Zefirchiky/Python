7e1a75b1d5b395178efd18ce5a0d17c35b4aabdd
zefirchiky <mudruk.arte@gmail.com>
2023-02-27 10:50:16 +0100

    MAIN all

---
+++ BIOLOGY.py
@@ -0,0 +1,13 @@
+from time import sleep
+
+x=1
+while 1:
+    print(x*"o")
+    sleep(.2)
+    print(x*"O")
+    sleep(.2)
+    print(x*"0")
+    sleep(.2)
+    print(x*"8")
+    sleep(.2)
+    x*=2
