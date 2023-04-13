6950ba9f656011c5977ab75ffa79b503c96730c7
Mudruk Artem <74060222+Zefirchiky@users.noreply.github.com>
2023-04-13 12:16:01 +0300

    update

--- F.py
+++ F.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 while 1:
     n=input("Символ:")
     k=len(n)
@@ -17,23 +16,3 @@ while 1:
         print(n*k*2)
     for i in range(6):
         print(n*k)
-=======
-while 1:
-    n=input("Символ:")
-    k=len(n)
-    if k<=6:
-        k=round(6/k)
-        print(k)
-    else:
-        k=1
-        u=len(n)-6
-        n=n[:-u]
-    for i in range(3):
-        print(n*k*3)
-    for i in range(3):
-        print(n*k)
-    for i in range(3):
-        print(n*k*2)
-    for i in range(6):
-        print(n*k)
->>>>>>> d4a9429d8f2eeba9daf04fa2d85d8eb63d449416
--- ImagesVimer.py
+++ ImagesVimer.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from time import sleep
 import pygame as pg
 
@@ -10,17 +9,5 @@ for i in range(9000):
     img = pg.image.load(f"D:\Artem\ijknhbg\Bests\{i}")
     img_s = img.get_size()
     DIS.blit(img, (W/2 - img_s[0]/2, H/2 - img_s[1]/2))
-=======
-from time import sleep
-import pygame as pg
 
-W, H = 1500, 1100
-DIS = pg.display.set_mode((W, H))
-pg.display.set_caption("LOL")
-
-for i in range(9000):
-    img = pg.image.load(f"D:\Artem\ijknhbg\Bests\{i}")
-    img_s = img.get_size()
-    DIS.blit(img, (W/2 - img_s[0]/2, H/2 - img_s[1]/2))
->>>>>>> d4a9429d8f2eeba9daf04fa2d85d8eb63d449416
     
